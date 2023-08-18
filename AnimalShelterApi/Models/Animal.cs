0c063bc30533688ba1f21aa40d38f3db46380556
Lindsay Warr <iamalion@gmail.com>
2023-08-18 09:22:25 -0700

    Add custom models, delete boilerplate

---
+++ AnimalShelterApi/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelterApi.Models
+{
+    public class AnimalShelterApi
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public bool Available { get; set; }
+    }
+}
