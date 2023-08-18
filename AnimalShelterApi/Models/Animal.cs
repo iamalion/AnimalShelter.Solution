0eee61cbc40f10b13963453536e653f23804c9c3
Lindsay Warr <iamalion@gmail.com>
2023-08-18 09:43:34 -0700

    Add initial database migration

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -1,10 +1,11 @@
 namespace AnimalShelterApi.Models
 {
-    public class AnimalShelterApi
+    public class Animal
     {
         public int AnimalId { get; set; }
         public string Name { get; set; }
         public string Species { get; set; }
+        public string Breed { get; set; }
         public int Age { get; set; }
         public string Description { get; set; }
         public bool Available { get; set; }
